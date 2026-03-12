7486d32210bee489cfed31f2d89835308ad54222
HuNorbert <szino1848@gmail.com>
2026-03-12 08:14:39 +0100

    debug RD_v2 2

--- UPS_PoD_Letolto_RD_v2.ps1
+++ UPS_PoD_Letolto_RD_v2.ps1
@@ -101,9 +101,13 @@ $launchChromeButton.ForeColor = "White"
 $launchChromeButton.Font = New-Object System.Drawing.Font("Arial", 10, [System.Drawing.FontStyle]::Bold)
 $launchChromeButton.Cursor = [System.Windows.Forms.Cursors]::Hand  # [1] Kéz kurzor
 $launchChromeButton.Add_Click({
-    # Elso lepés: minden Chrome folyamat leolese
-    Clear-AllChromeProcesses
-    Start-Sleep -Seconds 1
+    $portCheck = Test-NetConnection -ComputerName 127.0.0.1 -Port 9222 -WarningAction SilentlyContinue -InformationLevel Quiet
+    if ($portCheck) {
+        Write-Log "POD Chrome mar fut a 9222-es porton."
+        $chromeStatus.Text = "✓ POD Chrome fut"
+        $chromeStatus.ForeColor = "DarkGreen"
+        return
+    }
 
     $chromePaths = @(
         "C:\Program Files\Google\Chrome\Application\chrome.exe",
